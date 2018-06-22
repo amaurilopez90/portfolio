1ce9c85d36caa83083e6bc489c1f16e2b8034e63
Amauri Lopez <lopeza16@tcnj.edu>
2018-06-22 15:54:09 -0400

    Migrated to react-hot-loader

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+import { hot, AppContainer } from 'react-hot-loader';
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
+
+export default hot(module)(App)
